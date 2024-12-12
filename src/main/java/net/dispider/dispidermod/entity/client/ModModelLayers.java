5d0741841c5955fa9cf3c4d699b0205215ae66a7
Tarantel <SerienKid@live.de>
2024-12-12 17:26:03 +0100

    Update ModModelLayers.java

--- src/main/java/net/dispider/dispidermod/entity/client/ModModelLayers.java
+++ src/main/java/net/dispider/dispidermod/entity/client/ModModelLayers.java
@@ -5,6 +5,16 @@ import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.resources.ResourceLocation;
 
 public class ModModelLayers {
-    public static final ModelLayerLocation GRON_LAYER = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(DiSpiderMod.EXAMPLEMOD
-    ,"gron_layer"),"main");
+
+
+
+    public static final ModelLayerLocation GRON_LAYER = register("gron_layer");
+
+    private static ModelLayerLocation register(String name) {
+        return new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(DiSpiderMod.MODID, name), "main");
+    }
+
+    private static ModelLayerLocation register(String name, String layerName) {
+        return new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(DiSpiderMod.MODID, name), layerName);
+    }
  }
