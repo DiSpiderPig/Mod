70a80bf08042aa867051bd87898dfc820e471b04
Tarantel <SerienKid@live.de>
2024-12-12 17:33:16 +0100

    Add files via upload

--- src/main/java/net/dispider/dispidermod/worldgen/tree/ModTreeGrowers.java
+++ src/main/java/net/dispider/dispidermod/worldgen/tree/ModTreeGrowers.java
@@ -7,11 +7,11 @@ import net.minecraft.world.level.block.grower.TreeGrower;
 import java.util.Optional;
 
 public class ModTreeGrowers {
-    public static final TreeGrower BLUE = new TreeGrower(DiSpiderMod.EXAMPLEMOD + ":blue",
+    public static final TreeGrower BLUE = new TreeGrower(DiSpiderMod.MODID + ":blue",
             Optional.empty(), Optional.of(ModConfiguredFeatures.BLUE_KEY), Optional.empty());
-    public static final TreeGrower WHITE = new TreeGrower(DiSpiderMod.EXAMPLEMOD + ":white",
+    public static final TreeGrower WHITE = new TreeGrower(DiSpiderMod.MODID + ":white",
             Optional.empty(), Optional.of(ModConfiguredFeatures.WHITE_KEY), Optional.empty());
 
-    public static final TreeGrower RED = new TreeGrower(DiSpiderMod.EXAMPLEMOD + ":red",
+    public static final TreeGrower RED = new TreeGrower(DiSpiderMod.MODID + ":red",
             Optional.empty(), Optional.of(ModConfiguredFeatures.WHITE_KEY), Optional.empty());
 }
